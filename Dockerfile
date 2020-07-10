0cc6fe43d29caddd6399d8a0657e1bfd1056c29f
Hugothms <hugothms@gmail.com>
2020-07-10 12:50:29 +0200

    hugo

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ RUN apt-get update &&\
 	apt-get install -y wget nginx default-mysql-server php php-mbstring php-fpm php-mysql openssl nano
 
 COPY srcs/start.sh				srcs/
+COPY srcs/index.htm				srcs/
+COPY srcs/init_mysql.sql		srcs/
 COPY srcs/wordpress.tar.gz		var/www/html/
 COPY srcs/phpmyadmin.tar		var/www/html/
 COPY srcs/defaulton				etc/nginx/sites-available/
-
 
 EXPOSE 80 443
 
--- srcs/start.sh
+++ srcs/start.sh
@@ -1,11 +1,30 @@
+#!/bin/bash
+
+echo "Unzip PHP & Wordpress"
 cd var/www/html/
 tar -xf phpmyadmin.tar
 tar -xzf wordpress.tar.gz
 rm -rf wordpress.tar.gz phpmyadmin.tar
-# rm index.nginx-debian.html
 
-service nginx start
-service php7.3-fpm start
+echo "Set autoindex"
+rm /var/www/html/index.*
+cd /etc/nginx/sites-available
+autoindex=`cat default | grep autoindex`
+if [ "$autoindex" != "		autoindex on;" ]
+then
+	cp /srcs/index.htm /var/www/html/
+fi
+
+echo "Start MySQL"
 service mysql start
+mysql -u root -p123 < /srcs/init_mysql.sql
+
+echo "Start PHP"
+service php7.3-fpm start
+
+echo "Start Nginx"
+service nginx start
 # service mysql-u-p123 start
+
+cd /
 bash
